7ab8602b99b87c65d2cf9b1635e119867389da3c
alex <alex@pototskiy.net>
2019-02-27 19:39:42 +1000

    #3 config build env

--- mage-mediation/src/test/kotlin/net/pototskiy/apps/magemediation/loader/ExposedConnectionTest.kt
+++ mage-mediation/src/test/kotlin/net/pototskiy/apps/magemediation/loader/ExposedConnectionTest.kt
@@ -2,6 +2,9 @@ package net.pototskiy.apps.magemediation.loader
 
 import com.mysql.cj.jdbc.MysqlDataSource
 import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.IntIdTable
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
@@ -17,6 +20,11 @@ class ExposedConnectionTest {
         val str = varchar("str", 10)
     }
 
+    class TestEntity(id: EntityID<Int>): IntEntity(id) {
+        companion object : IntEntityClass<TestEntity>(TestTable)
+        var str by TestTable.str
+    }
+
     @Test
     @DisplayName("Connect to DB create and drop table")
     internal fun test() {
@@ -27,6 +35,14 @@ class ExposedConnectionTest {
         datasource.serverTimezone = TimeZone.getDefault().id
         Database.connect(datasource)
         transaction { SchemaUtils.create(TestTable) }
+        transaction {
+            TestEntity.new {
+                str = "123456"
+            }
+        }
+        val data = transaction { TestEntity.all().toList() }
+        assertThat(data).hasSize(1)
+        assertThat(data.first().str).isEqualTo("123456")
         transaction { SchemaUtils.drop(TestTable) }
         assertThat(true).isTrue()
     }
