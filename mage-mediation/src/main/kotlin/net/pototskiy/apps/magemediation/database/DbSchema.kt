e8a999fe533412adb91fb10d2c4e4e21bebb5865
alex <alex@pototskiy.net>
2019-02-27 21:21:27 +1000

    #3 config build env

--- mage-mediation/src/main/kotlin/net/pototskiy/apps/magemediation/database/DbSchema.kt
+++ mage-mediation/src/main/kotlin/net/pototskiy/apps/magemediation/database/DbSchema.kt
@@ -1,14 +1,18 @@
 package net.pototskiy.apps.magemediation.database
 
+import net.pototskiy.apps.magemediation.api.EXPOSED_LOG_NAME
 import net.pototskiy.apps.magemediation.api.STATUS_LOG_NAME
 import net.pototskiy.apps.magemediation.api.database.DbEntityTable
+import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.core.config.Configurator
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object DbSchema {
     fun createSchema() {
         val statusLog = LogManager.getLogger(STATUS_LOG_NAME)
+        Configurator.setLevel(EXPOSED_LOG_NAME, Level.DEBUG)
         transaction {
                 statusLog.info("schema: start")
                 SchemaUtils.create(DbEntityTable)
