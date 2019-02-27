0920934b3d40bcf5b53f1112d303225715d1ed11
alex <alex@pototskiy.net>
2019-02-27 19:53:31 +1000

    #3 config build env

--- mage-mediation/src/main/kotlin/net/pototskiy/apps/magemediation/database/DbSchema.kt
+++ mage-mediation/src/main/kotlin/net/pototskiy/apps/magemediation/database/DbSchema.kt
@@ -1,7 +1,6 @@
 package net.pototskiy.apps.magemediation.database
 
 import net.pototskiy.apps.magemediation.api.STATUS_LOG_NAME
-import net.pototskiy.apps.magemediation.api.database.*
 import org.apache.logging.log4j.LogManager
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -14,7 +13,15 @@ object DbSchema {
                 statusLog.info("schema: start")
 //                create(DbEntityTable)
                 statusLog.info("schema: 1 finished")
-/                statusLog.info("schema: 2 finished")
+//                create(
+//                    EntityVarchars,
+//                    EntityLongs,
+//                    EntityBooleans,
+//                    EntityDoubles,
+//                    EntityDateTimes,
+//                    EntityTexts
+//                )
+                statusLog.info("schema: 2 finished")
                 create(PipelineSets)
                 statusLog.info("schema: 3 finished")
             }
