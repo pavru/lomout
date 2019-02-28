64f414f765646984f296d04be12296b5f4158a47
alex <alex@pototskiy.net>
2019-02-28 19:16:38 +1000

    #3 config build env

--- mage-mediation/build.gradle.kts
+++ mage-mediation/build.gradle.kts
@@ -85,6 +85,9 @@ tasks.named<Test>("test") {
 //        events("passed", "skipped", "failed")
         events("passed", "skipped", "failed", "standardOut", "standardError")
     }
+    filter {
+        includeTestsMatching("*DataLoadingInProductionWayTest*")
+    }
 }
 
 tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).all {
