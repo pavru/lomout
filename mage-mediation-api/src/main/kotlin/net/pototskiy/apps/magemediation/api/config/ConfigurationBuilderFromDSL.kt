1f0dd01f6727aa369966051442b4f4a91ab24806
alex <alex@pototskiy.net>
2019-03-14 12:54:38 +1000

    oooast-tools-42 fixed, enough for now

--- mage-mediation-api/src/main/kotlin/net/pototskiy/apps/magemediation/api/config/ConfigurationBuilderFromDSL.kt
+++ mage-mediation-api/src/main/kotlin/net/pototskiy/apps/magemediation/api/config/ConfigurationBuilderFromDSL.kt
@@ -3,6 +3,9 @@ package net.pototskiy.apps.magemediation.api.config
 import net.pototskiy.apps.magemediation.api.STATUS_LOG_NAME
 import org.apache.logging.log4j.LogManager
 import java.io.File
+import kotlin.script.experimental.api.asSuccess
+import kotlin.script.experimental.api.onFailure
+import kotlin.script.experimental.api.onSuccess
 import kotlin.system.exitProcess
 
 class ConfigurationBuilderFromDSL(private val configFile: File) {
@@ -16,17 +19,23 @@ class ConfigurationBuilderFromDSL(private val configFile: File) {
         }
 
     private fun readConfig(): Config {
+        lateinit var evaluatedConfig: Config
         statusLog.info("Configuration loading has started from file: ${configFile.absolutePath}")
-        val config = try {
-            val configHost = ConfigHost(configFile)
-            configHost.compile()
-            configHost.evaluate()
-        } catch (e: ConfigException) {
-            logger.error(createConfigExceptionMessage(e))
-            exitProcess(1)
+        val configHost = ConfigHost(configFile)
+        configHost.compile().onFailure {
+            logger.error("Configuration file can not be compiled")
+        }.onSuccess { compileResult ->
+            configHost.evaluate().onFailure {
+                logger.error("Script can not be evaluated")
+                exitProcess(1)
+            }.onSuccess { evaluationResult ->
+                evaluatedConfig = evaluationResult
+                evaluationResult.asSuccess()
+            }
+            compileResult.asSuccess()
         }
         statusLog.info("Configuration loading has finished")
-        return config
+        return evaluatedConfig
     }
 
     private fun createConfigExceptionMessage(e: ConfigException): String {
